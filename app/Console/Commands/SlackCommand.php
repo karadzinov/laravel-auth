dc1e094b0eea709024875be272351925df4c16f1
martin <martin@myhost.mk>
2018-07-22 21:12:53 +0200

    slack command - clear

--- app/Console/Commands/SlackCommand.php
+++ app/Console/Commands/SlackCommand.php
@@ -55,9 +55,9 @@ class SlackCommand extends BaseHandler
         $message['message'] = ltrim($theMessage);
         $message['user_id'] = $this->guid;
         $message['flag']    = 'server';
-        if($message['message'] == "clear")
+        if(ltrim($theMessage) == "clear")
         {
-            Message::where('user_id', '=', $this->guid)->truncate();
+            Message::where('user_id', '=', $this->guid)->delete();
         }
         $message->save();
 
